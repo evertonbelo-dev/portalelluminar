dca674e534f254c3b6f83e346da5f8002903c432
everton.belo <everton.belo@gmail.com>
2026-01-27 16:47:09 +0000

    modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -25,16 +25,14 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          # Usamos um servidor simples de Python para projetar o seu HTML
+          command = ["python3" "-m" "http.server" "$PORT"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
       };
     };
 
